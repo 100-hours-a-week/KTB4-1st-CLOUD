a908445cd62f12cd9d4b65a90f92fbb352e94b86
dohyunk58 <hedge3x@gmail.com>
2026-09-23 15:23:20 +0900

    www 서브도메인 추가
    - 루트 도메인을 바라보도록 CNAME 레코드 설정

--- infra/v1/dns.tf
+++ infra/v1/dns.tf
@@ -10,3 +10,12 @@ resource "aws_route53_record" "v1" {
   ttl     = 300
   records = [aws_eip.v1.public_ip]
 }
+
+# www 서브도메인, 루트도메인을 바라보도록 CNAME 레코드 설정
+resource "aws_route53_record" "www" {
+  zone_id = data.aws_route53_zone.v1.zone_id
+  name    = "www.${var.domain_name}"
+  type    = "CNAME"
+  ttl     = 300
+  records = [var.domain_name]
+}
